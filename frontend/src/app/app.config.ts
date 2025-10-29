d026b86372465e53220adaddff934ee47883c1d7
Jose Imhoff <jose-imhoff@hotmail.com>
2025-10-28 23:58:04 -0300

    feat(frontend); make other configurations

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from './app-routing.module';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(HttpClientModule) // Proveedor para HttpClient
+  ]
+};
